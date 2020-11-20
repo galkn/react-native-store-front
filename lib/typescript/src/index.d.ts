a1eb85ef1ffed9c3c9cdec5f043eadc0bce34a07
Alessandro Gerelli <geroale2000@gmail.com>
2020-11-20 21:55:42 +0100

    Fixes

--- lib/typescript/src/index.d.ts
+++ lib/typescript/src/index.d.ts
@@ -1,5 +1,4 @@
-declare type StoreFrontType = {
-    multiply(a: number, b: number): Promise<number>;
+declare const _default: {
+    getStoreFront: () => Promise<string | undefined>;
 };
-declare const _default: StoreFrontType;
 export default _default;
