0b89639a1eef163bcbbc8dfff53467f2552d7d2c
Alessandro Gerelli <geroale2000@gmail.com>
2020-11-20 22:15:19 +0100

    Fixes

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@ import { NativeModules, Platform } from 'react-native';
 
 const { StoreFront } = NativeModules;
 
-console.log(StoreFront)
-
 const getStoreFront = async (): Promise<string | undefined> => {
   return Platform.select({
     ios: await StoreFront.getStoreFront() as string,
